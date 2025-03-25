602792ceb2705381a9616b95daad80f130a1c616
svakhilr <svakhilr@gmail.com>
2025-03-25 15:31:32 +0530

    removed gis

--- vendor/admin.py
+++ vendor/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from .models import Cusines,Restaurant,FoodItem,Category
-from leaflet.admin import LeafletGeoAdmin
 
-class RestaurantAdmin(LeafletGeoAdmin):
+
+class RestaurantAdmin(admin.ModelAdmin):
     pass
 
 class FoodItemAdmin(admin.ModelAdmin):
